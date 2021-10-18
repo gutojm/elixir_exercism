fe3c26046ae440fc6e19dae0a3596d18da18fc88
guto <gutojm@gmail.com>
2021-10-18 16:03:28 -0300

    add first exercices

---
+++ lasagna/lib/lasagna.ex
@@ -0,0 +1,11 @@
+defmodule Lasagna do
+  def expected_minutes_in_oven, do: 40
+
+  def remaining_minutes_in_oven(n), do: expected_minutes_in_oven() - n
+
+  def preparation_time_in_minutes(layers), do: layers * 2
+
+  def total_time_in_minutes(layers,minutes_in_oven), do: preparation_time_in_minutes(layers) + minutes_in_oven
+
+  def alarm, do: "Ding!"
+end
